e52d9f7d5e84c0c0441d2a3d9fb2101b84f03cde
Jose Pagan <jose.pagan@terex.com>
2025-06-19 10:35:38 -0700

    Adding Hello Word into utilities

---
+++ utilities/name.cpp
@@ -0,0 +1,15 @@
+/*****************************************************
+ **
+ ** Compile with:
+ **     g++ -std=c++11 name.cpp -o name_compiled
+ **
+ *****************************************************/
+
+#include <iostream>
+
+int main()
+{
+    printf("Hello, ROS developer!   \n");
+    
+    return 0;
+}
